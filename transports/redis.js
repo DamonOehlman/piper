873356fbaaaf27cc4a9320958acdf4750df43ff4
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-12 11:11:56 +1000

    Update indent to 2 spaces

--- transports/redis.js
+++ transports/redis.js
@@ -1,30 +1,30 @@
 var redis = require('redis');
 
 module.exports = function(channel, host, port, opts) {
-    // create the redis connection
-    var client = redis.createClient(host, port, opts);
-    
-    // ensure the channel has been specified
-    channel = channel || 'eve-piper';
+  // create the redis connection
+  var client = redis.createClient(host, port, opts);
 
-    return {
-        send: function(msg) {
-            client.publish(channel, msg);
-        },
-        
-        sub: function(handler) {
-            if (handler) {
-                client.subscribe(channel);
-                client.on('message', function(msgChannel, message) {
-                    if (msgChannel === channel) {
-                        handler(message);
-                    }
-                });
-            }
-        },
-        
-        unsub: function() {
-            client.unsubscribe(channel);
-        }
-    };
+  // ensure the channel has been specified
+  channel = channel || 'eve-piper';
+
+  return {
+    send: function(msg) {
+      client.publish(channel, msg);
+    },
+
+    sub: function(handler) {
+      if (handler) {
+        client.subscribe(channel);
+        client.on('message', function(msgChannel, message) {
+          if (msgChannel === channel) {
+            handler(message);
+          }
+        });
+      }
+    },
+
+    unsub: function() {
+      client.unsubscribe(channel);
+    }
+  };
 };
