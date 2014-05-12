d44c9b3926ced614974eed5b4a3727cd7973cd35
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-12 11:01:16 +1000

    Use piper rather than helper

--- test/namespacing.js
+++ test/namespacing.js
@@ -1,14 +1,14 @@
 var expect = require('expect.js'),
-    one = require('./helpers/piper')('1'),
-    two = require('./helpers/piper')('2');
-    
+    one = require('../')('1'),
+    two = require('../')('2');
+
 describe('events raised in one namespace are not triggered in the other', function() {
     it('namespaced functions are valid and different', function() {
         expect(one.on).to.be.ok();
         expect(two.on).to.be.ok();
         expect(one).to.not.equal(two);
     });
-    
+
     it('one should not capture events raised in two', function(done) {
         one.on('hi', function() {
             throw new Error('Should not have got this event');
@@ -18,7 +18,7 @@ describe('events raised in one namespace are not triggered in the other', functi
         setTimeout(done, 100);
         two('hi');
     });
-    
+
     it('two should not capture events raised by one', function(done) {
         two.on('ho', function() {
             throw new Error('Should not have got this event');
