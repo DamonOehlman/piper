35dfc272a05b247bdbe2eee7169439b6e7cbe6c6
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-12 11:39:07 +1000

    Make example more readable

--- examples/transports/redis.js
+++ examples/transports/redis.js
@@ -1,8 +1,14 @@
-var piper = require('piper'),
-    testpipe = piper('test');
+var piper = require('../../');
+var testpipe = piper('test');
 
 // publish events via redis
-piper.bridge(piper.createTransport('redis')).pub();
+var bridge = piper.bridge(require('../../transports/redis')({
+  host: 'localhost',
+  channel: 'redis-test-channel'
+}));
+
+// start the bridge publishing
+bridge.pub();
 
 // fire an event
 testpipe('hit', 'car');
